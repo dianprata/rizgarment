ee01d11ad828b11df6f5793d9c3330d0594f6a5a
Unknown <dianpratama2@gmail.com>
2017-08-16 17:56:21 +0800

    Add theme file to repository

---
+++ assets/js/app.js
@@ -0,0 +1,51 @@
+/*
+ * Application
+ */
+
+import $ from "jquery"
+import "./../../node_modules/bootstrap-sass/assets/javascripts/bootstrap"
+
+$(document).tooltip({
+    selector: "[data-toggle=tooltip]"
+})
+
+/*
+ * Auto hide navbar
+ */
+jQuery(document).ready(function($){
+    var $header = $('.navbar-autohide'),
+        scrolling = false,
+        previousTop = 0,
+        currentTop = 0,
+        scrollDelta = 10,
+        scrollOffset = 150
+
+    $(window).on('scroll', function(){
+        if (!scrolling) {
+            scrolling = true
+
+            if (!window.requestAnimationFrame) {
+                setTimeout(autoHideHeader, 250)
+            }
+            else {
+                requestAnimationFrame(autoHideHeader)
+            }
+        }
+    })
+
+    function autoHideHeader() {
+        var currentTop = $(window).scrollTop()
+
+        // Scrolling up
+        if (previousTop - currentTop > scrollDelta) {
+            $header.removeClass('is-hidden')
+        }
+        else if (currentTop - previousTop > scrollDelta && currentTop > scrollOffset) {
+            // Scrolling down
+            $header.addClass('is-hidden')
+        }
+
+        previousTop = currentTop
+        scrolling = false
+    }
+});
